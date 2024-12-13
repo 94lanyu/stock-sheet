5f72afdb10e5c2684833db83faf5f50308b3056a
懶魚 <189705063+94lanyu@users.noreply.github.com>
2024-12-13 12:22:24 +0800

    【Hope主題】v35 修正 sitemap 排除路徑

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -88,7 +88,8 @@ export default hopeTheme({
 
         sitemap: {
             excludeUrls: [
-                "/demo/"
+                "/demo/",
+                "/demo/*"
             ],
         },
 
