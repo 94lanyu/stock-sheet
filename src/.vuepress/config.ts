bc93f6bb4318bbd57e215a89236eed938cbd8609
懶魚 <189705063+94lanyu@users.noreply.github.com>
2025-03-29 21:07:24 +0800

    【Hope主題】v45 config base 復原成跟目錄 '/'

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,16 @@
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  // base: "/StockSheet/",
+    // base: "/StockSheet/",
+    base: '/',
 
-  lang: "zh-TW",
-  title: "全自動股票管理表",
-  description: "自動化股票管理excel(Google試算表)，免下載，複製立即可用。免費試用、簡單股票記帳、股票管理表、自動更新股價、股票儀表版",
+    lang: "zh-TW",
+    title: "全自動股票管理表",
+    description: "自動化股票管理excel(Google試算表)，免下載，複製立即可用。免費試用、簡單股票記帳、股票管理表、自動更新股價、股票儀表版",
 
-  theme,
+    theme,
 
-  // Enable it with pwa
-  // shouldPrefetch: false,
+    // Enable it with pwa
+    // shouldPrefetch: false,
 });
