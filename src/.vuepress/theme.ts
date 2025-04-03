25793069e0bee96375ade6410d93f0c6b6d5fcb1
懶魚 <189705063+94lanyu@users.noreply.github.com>
2025-04-03 18:24:06 +0800

    【Cloudflare】修改 hostname

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-    hostname: "https://94lanyu.github.io",
+    hostname: "https://stock-sheet.pages.dev",
 
     author: {
         name: "懶魚",
